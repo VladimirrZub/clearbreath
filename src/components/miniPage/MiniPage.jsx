d4f006f61d8650b9082d9a8abc1f362d4445872d
Vovka <copp@MacBook-Air-COPP.local>
2025-03-06 09:41:46 +0300

    sisks

--- src/components/miniPage/MiniPage.jsx
+++ src/components/miniPage/MiniPage.jsx
@@ -4,20 +4,20 @@ import s from '../miniPage/MiniPage.module.css'
 const MiniPage = () => {
 	return (
 		<div className={s.mainContainer}>
-			<div className={s.containerContain}>
-				<div className={s.containText}>
-					Оставьте заявку и мы с вами свяжемся
-				</div>
-				<div className={s.containInputs}>
-					<div className={s.inputName}></div>
-					<div className={s.inputPhone}></div>
-					<div className={s.inputBTN}></div>
-					<button className={s.inputBTN}>
-						<button className={s.BTNSecStyle}>ОТПРАВИТЬ</button>
-					</button>
-				</div>
-			</div>
-		</div>
+      <div className={s.containerContain}>
+        <div className={s.containText}>
+          Оставьте заявку и мы с вами свяжемся
+        </div>
+        <div className={s.containInputs}>
+          <input type='text' id='inputName' name='inputName' required></input>
+          <input type='tel' id='inputPhone' name='inputPhone' required></input>
+          <button className={s.inputBTN}>
+            <button className={s.BTNSecStyle}>ОТПРАВИТЬ</button>
+          </button>
+        </div>
+      </div>
+    </div>
+
 	)
 }
 
