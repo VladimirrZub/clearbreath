3a3a785237984cc1fcfab884fff56eb77cb229e4
Vovka <copp@MacBook-Air-COPP.local>
2025-03-06 11:18:01 +0300

    ahaha

--- src/components/miniPage/MiniPage.jsx
+++ src/components/miniPage/MiniPage.jsx
@@ -4,20 +4,33 @@ import s from '../miniPage/MiniPage.module.css'
 const MiniPage = () => {
 	return (
 		<div className={s.mainContainer}>
-      <div className={s.containerContain}>
-        <div className={s.containText}>
-          Оставьте заявку и мы с вами свяжемся
-        </div>
-        <div className={s.containInputs}>
-          <input type='text' id='inputName' name='inputName' required></input>
-          <input type='tel' id='inputPhone' name='inputPhone' required></input>
-          <button className={s.inputBTN}>
-            <button className={s.BTNSecStyle}>ОТПРАВИТЬ</button>
-          </button>
-        </div>
-      </div>
-    </div>
-
+			<div className={s.containerContain}>
+				<div className={s.containText}>
+					Оставьте заявку и мы с вами свяжемся
+				</div>
+				<div className={s.containInputs}>
+					<input
+						type='text'
+						name='inputName'
+						placeholder='Введите текст здесь'
+						onclick="this.placeholder=''"
+						onblur="if (this.value == '') {this.placeholder='Введите текст здесь';}"
+						required
+					></input>
+					<input
+						type='tel'
+						name='inputTel'
+						placeholder='Введите текст здесь'
+						onclick="this.placeholder=''"
+						onblur="if (this.value == '') {this.placeholder='Введите текст здесь';}"
+						required
+					></input>
+					<button className={s.inputBTN}>
+						<button className={s.BTNSecStyle}>ОТПРАВИТЬ</button>
+					</button>
+				</div>
+			</div>
+		</div>
 	)
 }
 
